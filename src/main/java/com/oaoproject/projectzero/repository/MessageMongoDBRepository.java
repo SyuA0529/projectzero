0c2601de61b525b740d1284cc7e664cfefbe93b3
SyuA <silbi3408@gmail.com>
2023-04-09 21:49:15 +0900

    오타 수정

--- src/main/java/com/oaoproject/projectzero/repository/MessageMongoDBRepository.java
+++ src/main/java/com/oaoproject/projectzero/repository/MessageMongoDBRepository.java
@@ -7,14 +7,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MessageMongoDBRepository extends MongoRepository<Message, String> {
-    // create
     public Optional<Message> findById(String id);
     public List<Message> findByMessageBodyContaining(String fraction);
     public List<Message> findByMemberId(String memberId);
     public List<Message> findByLatitudeBetweenAndLongitudeBetween(Long latitude1, Long latitude2,
                                                                   Long longitude1, Long longitude2);
 
-    // delete
     public void deleteByMemberId(String memberId);
 
 }
